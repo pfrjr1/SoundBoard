7a5f475bc8f0105da94636e04c664fc48bb502c6
Peter Rooney <pfrjr1@me.com>
2017-05-04 10:15:01 -0400

    end of lesson 88

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Peter Rooney on 5/4/17.
+//  Copyright © 2017 Peter Rooney. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addtapped(_ sender: Any) {
+    }
+    
+}
