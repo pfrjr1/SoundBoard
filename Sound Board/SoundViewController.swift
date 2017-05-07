f3ec98d57422e3e7c0d0423d1c3d51fb765c027b
Peter Rooney <pfrjr1@me.com>
2017-05-07 08:27:46 -0400

    end of lesson 91 i had to use MP4AAC v2? but it worked after adding any object

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder? = nil
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupRecorder()
+    }
+    
+    func setupRecorder() {
+        
+        do {
+        
+        // Create an audio session 
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try session.overrideOutputAudioPort(.speaker)
+        try session.setActive(true)
+        
+        // Create URL for the audio file
+            
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.
+                documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+        
+        // Create settings for the audio recorder
+            
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC_ELD_V2) as AnyObject
+            settings[AVSampleRateKey] = 44100.0 as AnyObject
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject
+            
+        
+        // Create AudioRecorder object
+        
+        audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            audioRecorder!.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+            }
     }
 
     @IBAction func recordTapped(_ sender: Any) {
