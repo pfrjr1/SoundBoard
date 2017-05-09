c25d4df79604996228d5f5c15d001a4a480138c1
Peter Rooney <pfrjr1@me.com>
2017-05-09 08:52:42 -0400

    end of lecture 95 all good

--- Sound Board/ViewController.swift
+++ Sound Board/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+   
+    
+    var sounds : [Sound] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as!
+            AppDelegate).persistentContainer.viewContext
+        do{
+        
+        sounds = try context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        }catch{}
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let sound = sounds[indexPath.row]
+        
+        cell.textLabel?.text = sound.name
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+ }
 
